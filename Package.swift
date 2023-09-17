2ec9b1e70b7604e07c206ef69bbe8d9c174dee2a
Valentin <36477338+ValentinWalter@users.noreply.github.com>
2023-09-17 19:26:13 +0200

    Make minimum platforms consistent with other packages

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Dated",
-    platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10), .visionOS(.v1)],
+    platforms: [.iOS(.v17), .macOS(.v14), .macCatalyst(.v17), .tvOS(.v17), .watchOS(.v10), .visionOS(.v1)],
     products: [
         .library(
             name: "Dated",
